37ff651d6d27437a83c70f575d37a17e3503f2ed
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2018-06-17 09:15:32 +0200

    Describe project metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule StashExchange.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "StashExchange",
+      description: "The single value storage",
+      source_url: github_link(),
+      package: package()
     ]
   end
 
@@ -23,5 +27,18 @@ defmodule StashExchange.MixProject do
     [
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
+  end
+
+  defp package() do
+    [
+      name: "stash_exchange", # Optional if we want to keep OTP app name
+      maintainers: ["Vitali Tatarintev"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => github_link()}
+    ]
+  end
+
+  defp github_link() do
+    "https://github.com/ck3g/stash_exchange"
   end
 end
